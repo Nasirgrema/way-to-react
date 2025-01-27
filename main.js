e96ae93f15c6c0c1a646fde49181a264bbe46712
Nasirgrema <muhammadnasirg250@gmail.com>
2025-01-27 13:08:51 +0100

    feat: Adding some folders

--- main.js
+++ main.js
@@ -134,17 +134,19 @@ let getIndexOfPersonWhoIsFromNigerria = personArray.findIndex((singlePerson)=>{
 console.log(getIndexOfPersonWhoIsFromNigerria);
 
 // Application programming interface API
-let getListOfProductsElements = document.querySelector('.list-of-products');
-
-async function fetchListOfProducts() {
+let getListOfProductsElement = document.querySelector(".list-of-products");
+function renderProducts(getProducts){
+    getListOfProductsElement.innerHTML = getProducts
+    .map((singleProductItem)=>`<p>${singleProductItem.title} </p>`).join("");
+}
+async function fetchProductList() {
     try{
-       const apiResponse = await fetch('https://dummyjson.com/products',{
-        method: "GET",
-       });
-       const result = await apiResponse.json();
-       console.log(result);
+      const apiResponse = await fetch("https://dummyjson.com/products",{method : 'GET',});
+      const result = await apiResponse.json();
+      console.log(result);
+      if(result?.products?.length >0) renderProducts(result?.products);
     }catch(e){
         console.log(e);
     }
 }
-fetchListOfProducts()
+fetchProductList();
